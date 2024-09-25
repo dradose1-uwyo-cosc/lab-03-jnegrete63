796276d3bb46c742ea45ed074a457b918b99bbec
jnegrete63 <jnegrete@uwyo.edu>
2024-09-25 00:39:18 +0000

    in class practice

---
+++ practice.py
@@ -0,0 +1,14 @@
+names = ["Jose", "Edmend" , "Eddy"]
+
+print(f"Original", names)
+
+#del names[0]
+names.remove("Jose")
+
+names.append("Nile")
+
+#loop "is on my table"
+
+for name in names:
+        print(f"{name} is on my table")
+        
